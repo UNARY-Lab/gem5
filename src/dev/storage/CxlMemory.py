358d0cff9ef518fd4a91b7ce76f74a511e19497c
Marco Kurzynski <marcokurzynski@icloud.com>
2023-10-13 22:57:32 +0000

    stop using IDE class code, and set memory bit by default

--- src/dev/storage/CxlMemory.py
+++ src/dev/storage/CxlMemory.py
@@ -13,16 +13,16 @@ class CxlMemory(PciDevice):
     cxl_mem_latency = Param.Latency(
         '2ns', "cxl.mem protocol processing's latency for device")
 
-    VendorID = 0x8086
-    DeviceID = 0x7890
-    Command = 0x0
-    # Status = 0x280
+    VendorID = 0x6969 # random number
+    DeviceID = 0x0420 # random number
+    Command = 0x2 # set memory bit
+    Status = 0x80 # set fast back-to-back bit ? not sure what it does
     Revision = 0x0
-    ClassCode = 0x01
-    SubClassCode = 0x01
-    ProgIF = 0x85
-    InterruptLine = 0x1f
-    InterruptPin = 0x01
+    ClassCode = 0xFF # Unassigned class
+    SubClassCode = 0x00 # N/A
+    ProgIF = 0x00 # N/A
+    InterruptLine = 0xFF # No connection
+    InterruptPin = 0x00 # No interrupt pin
 
     # Primary
     BAR0 = PciMemBar(size='128MiB')
